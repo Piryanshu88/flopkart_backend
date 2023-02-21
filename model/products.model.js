e6161a3869af4e43979fc411e9fa2bc759807066
Piryanshu88 <deepubisht2004@gmail.com>
2023-02-22 02:39:21 +0530

    adding backend project

---
+++ model/products.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const reqString = { type: String, required: true };
+
+const allProductsSchema = mongoose.Schema({
+  uniq_id: String,
+  crawl_timestamp: String,
+  product_url: String,
+  product_name: reqString,
+  product_category_tree: String,
+  pid: String,
+  retail_price: reqString,
+  discounted_price: reqString,
+  image: reqString,
+  is_FK_Advantage_product: String,
+  description: String,
+  product_rating: String,
+  overall_rating: String,
+  brand: reqString,
+  product_specifications: reqString,
+});
+
+const ProductModel = mongoose.model("product", allProductsSchema);
+
+module.exports = {
+  ProductModel,
+};
